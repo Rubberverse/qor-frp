765d1a7822f83263e23499746df522f6d41e004d
Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>
2025-03-10 02:14:47 +0100

    Remove GOSU env checking from entrypoint script
    Signed-off-by: Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -8,46 +8,23 @@ purple='\033[38;5;135m'
 green='\033[38;5;41m'
 blue='\033[38;5;99m'
 
-if [ "$(whoami)" = "root" ] && [ "$GOSU" = 1 ]; then 
-    printf "%b" "[entrypoint] Fixing ownership of (sub-)directories and files regardless of change in ownership\n"
-    chown -Rf "$CONT_UID":"$CONT_UID" /app
-
-    FILE_PATH=/usr/local/share/ca-certificates/my-cert.crt
-    cp /app/certs/cert.pem "$FILE_PATH"
-    cat "$FILE_PATH" >> /etc/ssl/certs/ca-certificates.crt
-    update-ca-certificates
-
-    printf "%b" "[entrypoint] Changing user using gosu\n"
-    exec /app/bin/gosu "$CONT_USER" /app/scripts/docker-entrypoint.sh
-
-# If user is using gosu variant and changing user account
-elif [ "$(whoami)" != "root" ] && [ "$GOSU" = 1 ]; then
-    printf "%b" "[❌ " "$pink" "entrypoint - Error" "$cend" "] This variant of the image must be run as root user!\n"
-    printf "%b" "[❌ " "$pink" "entrypoint - Error" "$cend" "] Please use rootless variant instead.\n"
-    exit 1
-fi
-
 # Configuration validity check block
 if [ -n "$CONFIG_PATH" ] && [ -f "$CONFIG_PATH" ]; then
     printf "%b" "[✨ " "$purple" "entrypoint - Pass" "$cend" "] ✅ CONFIG_PATH is valid!\n"
 else
-    printf "%b" "[❌ " "$pink" "entrypoint - Error" "$cend" "] No file or path was found in CONFIG_PATH environment variable\n"
-    printf "%b" "[❌ " "$pink" "entrypoint - Error" "$cend" "] Users are expected to mount their own configuration and point CONFIG_PATH environmental variable to it's location\n"
+    printf "%b" "[❌ " "$pink" "entrypoint - Error" "$cend" "] No file or path was found in CONFIG_PATH environment variable.\n"
+    printf "%b" "[❌ " "$pink" "entrypoint - Error" "$cend" "] E01: Configuration is invalid, unreadable by current user or non-existent.\n"
     exit 1
 fi
 
-# Binary check if block, probably doesn't work but who cares (well I kinda do but not enough to find out why)
-if [ -z "$FRP_TYPE" ]; then
-    printf "%b" "[✨" " $green" "entrypoint" "$cend" "] Checking binary files...\n"
-    printf "%b" "[✨ " "$purple" "entrypoint - Info" "$cend" "] Keep in mind that if your binary files are different name, you should point FRP_NAME to a valid name.\n"
-    printf "%b" "[✨ " "$purple" "entrypoint - Info" "$cend" "] You can always skip this check by setting FRP_TYPE to SERVER or CLIENT (use correct type for your image)\n"
-    command -v /app/bin/"$FRP_NAME" >/dev/null 2>&1 || export FRP_TYPE="CLIENT"
-    command -v /app/bin/"$FRP_NAME" >/dev/null 2>&1 || export FRP_TYPE="SERVER"
+if [ -w "$CONFIG_PATH" ]; then
+    printf "%b" "[⚠️ " "$darkorange" "entrypoint - Warning" "$cend" "] Your configuration file is writeable by the container!\n"
+    printf "%b" "[⚠️ " "$darkorange" "entrypoint - Warning" "$cend" "] It is recommended to mount it as read-only instead, unless you have a good reason for it!\n"
 fi
 
 printf "%b" "$darkorange" " ______        _     _                                             \n(_____ \      | |   | |                                            \n _____) )_   _| |__ | |__  _____  ____ _   _ _____  ____ ___ _____ \n|  __  /| | | |  _ \|  _ \| ___ |/ ___) | | | ___ |/ ___)___) ___ |\n| |  \ \| |_| | |_) ) |_) ) ____| |    \ V /| ____| |  |___ | ____|\n|_|   |_|____/|____/|____/|_____)_|     \_/ |_____)_|  (___/|_____)\n" "$cend";
 printf "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
-printf "%b" "🗒️ " "$blue" "Setup Guide " "$cend" "- https://github.com/rubberverse/qor-frp/Setup.md \n"
+printf "%b" "🗒️ " "$blue" "Setup Guide " "$cend" "- https://github.com/rubberverse/qor-frp/README.md \n"
 printf "%b" "📁 " "$green" "GitHub Repository " "$cend" "- https://github.com/rubberverse/qor-frp \n"
 printf "%b" "🦆 Hey there, thank you for using my images! In case you run into issues please report them on our GitHub repository\n"
 
@@ -57,4 +34,8 @@ if [ "$FRP_TYPE" = "CLIENT" ]; then
 elif [ "$FRP_TYPE" = "SERVER" ]; then
     printf "%b" "[✨" " $green" "entrypoint" "$cend" "] Starting frp server\n"
     exec /app/bin/frps -c "$CONFIG_PATH" "$EXTRA_ARGUMENTS"
+else
+    printf "%b" "[❌ " "$pink" "entrypoint - Error" "$cend" "] Do not modify FRP_TYPE environmental variable\n"
+    printf "%b" "[❌ " "$pink" "entrypoint - Error" "$cend" "] E02: FRP_TYPE is not CLIENT or SERVER\n"
+    exit 1
 fi
